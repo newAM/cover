2bd8db4edb6ad878b7214d5266c0c06bbffaec19
Alex Martens <alex@thinglab.org>
2022-10-11 16:33:27 -0700

    update lockfiles

--- flake.nix
+++ flake.nix
@@ -1,41 +1,79 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      mkPackage = system:
-        let
-          pkgs = nixpkgs.legacyPackages."${system}";
-          proc_cpuinfo = ./proc_cpuinfo.txt;
-        in
-        pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          # Hacks to fix: https://github.com/NixOS/nixpkgs/issues/122993
-          overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-            rpi-gpio = super.rpi-gpio.overridePythonAttrs (old: {
-              postPatch = ''
-                substituteInPlace source/cpuinfo.c \
-                  --replace "/proc/cpuinfo" "${proc_cpuinfo}"
-              '';
-            });
-            gpiozero = super.gpiozero.overridePythonAttrs (old: {
-              postPatch = ''
-                substituteInPlace gpiozero/pins/local.py \
-                  --replace "/proc/cpuinfo" "${proc_cpuinfo}"
-              '';
-            });
-          });
-        };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    mkPackage = system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      proc_cpuinfo = ./proc_cpuinfo.txt;
     in
-    {
-      overlays.default = final: prev: {
-        cover = self.packages.${prev.system}.default;
+      pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        # Hacks to fix: https://github.com/NixOS/nixpkgs/issues/122993
+        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+          rpi-gpio = super.rpi-gpio.overridePythonAttrs (old: {
+            postPatch = ''
+              substituteInPlace source/cpuinfo.c \
+                --replace "/proc/cpuinfo" "${proc_cpuinfo}"
+            '';
+          });
+          gpiozero = super.gpiozero.overridePythonAttrs (old: {
+            postPatch = ''
+              substituteInPlace gpiozero/pins/local.py \
+                --replace "/proc/cpuinfo" "${proc_cpuinfo}"
+            '';
+          });
+        });
       };
-      nixosModules.default = import ./module.nix;
-
-      packages = {
-        aarch64-linux.default = mkPackage "aarch64-linux";
-        x86_64-linux.default = mkPackage "x86_64-linux";
-      };
+  in {
+    overlays.default = final: prev: {
+      cover = self.packages.${prev.system}.default;
     };
+    nixosModules.default = import ./module.nix;
+
+    packages = {
+      aarch64-linux.default = mkPackage "aarch64-linux";
+      x86_64-linux.default = mkPackage "x86_64-linux";
+    };
+
+    checks.x86_64-linux = let
+      nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".nix"];
+      pySrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".py"];
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      pkg-aarch64-linux = self.packages.aarch64-linux.default;
+      pkg-x86_64-linux = self.packages.x86_64-linux.default;
+
+      black = pkgs.runCommand "black" {} ''
+        ${pkgs.python3Packages.black}/bin/black --config ${./pyproject.toml} ${pySrc}
+        touch $out
+      '';
+
+      flake8 =
+        pkgs.runCommand "flake8"
+        {
+          buildInputs = with pkgs.python3Packages; [
+            flake8
+            flake8-bugbear
+            pep8-naming
+          ];
+        }
+        ''
+          flake8 --max-line-length 88 ${pySrc}
+          touch $out
+        '';
+
+      alejandra = pkgs.runCommand "alejandra" {} ''
+        ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
+        touch $out
+      '';
+
+      statix = pkgs.runCommand "statix" {} ''
+        ${pkgs.statix}/bin/statix check ${nixSrc}
+        touch $out
+      '';
+    };
+  };
 }
--- module.nix
+++ module.nix
@@ -1,9 +1,11 @@
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.services.cover;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.cover;
+in {
   options.services.cover = with lib; {
     enable = mkEnableOption "cover";
 
@@ -16,17 +18,16 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.services.cover =
-      {
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "idle";
-          KillSignal = "SIGINT";
-          ExecStart = "${pkgs.cover}/bin/cover ${cfg.hostname}";
-          Restart = "on-failure";
-          RestartSec = 10;
-        };
-        environment.GPIOZERO_PIN_FACTORY = "native";
+    systemd.services.cover = {
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        Type = "idle";
+        KillSignal = "SIGINT";
+        ExecStart = "${pkgs.cover}/bin/cover ${cfg.hostname}";
+        Restart = "on-failure";
+        RestartSec = 10;
       };
+      environment.GPIOZERO_PIN_FACTORY = "native";
+    };
   };
 }
